9de7b488da8108065b400dee25ccf575acca59fb
halconrod <rodolfopalacio487@gmail.com>
2018-02-24 17:47:20 -0400

    ok

---
+++ resources/views/web/new-layouts/master.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<!--
+	Radius by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>SexyPeru 69 @yield('title')</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="{{ asset('web/css/main.css') }}" />
+	</head>
+	<body>
+
+		@yield('content')
+
+		<!-- Footer -->
+		<footer id="footer">
+			<a href="#" class="info fa fa-info-circle"><span>About</span></a>
+			<div class="inner">
+				<div class="content">
+					<h3>Sexy Peru 69</h3>
+					<p>Bienvenido al lugar donde todas tus fantasias seharan realidad.</p>
+				</div>
+				<div class="copyright">
+					<h3>Siguenos!</h3>
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+					</ul>
+					&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
+				</div>
+			</div>
+		</footer>
+
+	</body>
+	<!-- Scripts -->
+		<script src="{{ asset('web/js/jquery.min.js') }}"></script>
+		<script src="{{ asset('web/js/skel.min.js') }}"></script>
+		<script src="{{ asset('web/js/util.js') }}"></script>
+		<script src="{{ asset('web/js/main.js') }}"></script>
+</html>
